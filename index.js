45046cb9638a9a5afd8b575a89c27e664041a702
Arnab Mitra <112240598+mitrarnab0@users.noreply.github.com>
2024-07-08 11:22:46 +0000

    updated new

--- index.js
+++ index.js
@@ -45,4 +45,4 @@ app.get('*', (req, res) => {
 })
 
 // listening 
-app.listen('5000', () => console.log('\t Listening on port :3000'))
+app.listen('5000', () => console.log('\t Listening on port :5000'))
